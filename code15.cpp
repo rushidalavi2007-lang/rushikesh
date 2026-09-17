0717e3e2ad2a65ae5376ef834bf907f08053b83b
rushidalavi2007-lang <rushidalavi2007@gmail.com>
2026-09-17 23:16:17 +0530

    code15.cpp

---
+++ code15.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+
+class Academic {
+public:
+    void display() { 
+        cout << "Academic information" << endl; 
+    }
+};
+
+class Sports {
+public:
+    void display() { 
+        cout << "Sports information" << endl; 
+    }
+};
+
+class Student : public Academic, public Sports {
+public:
+    void displayAll() {
+        Academic::display();
+        Sports::display();
+    }
+};
+
+int main() {
+    Student s;
+    s.Academic::display();
+    s.Sports::display();
+    s.displayAll();
+    return 0;
+}
