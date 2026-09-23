e547902f900f3c44689589bce33b3f680cbce6fd
rushidalavi2007-lang <rushidalavi2007@gmail.com>
2026-09-23 21:08:34 +0530

    live example 10

---
+++ U3_Live_Example_02.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+using namespace std;
+
+class Complex {
+private:
+    double real, imag;
+public:
+    Complex(double r = 0.0, double i = 0.0) : real(r), imag(i) {}
+
+    Complex operator+(const Complex& other) const {
+        return Complex(real + other.real, imag + other.imag);
+    }
+
+    Complex operator-(const Complex& other) const {
+        return Complex(real - other.real, imag - other.imag);
+    }
+
+    void display() const {
+        cout << real << " + " << imag << "i" << endl;
+    }
+};
+
+int main() {
+    Complex c1(3.0, 4.0), c2(1.0, 2.0);
+    Complex c3 = c1 + c2;
+    Complex c4 = c1 - c2;
+
+    cout << "== Complex Calculator ==" << endl;
+    cout << "Sum: "; c3.display();
+    cout << "Difference: "; c4.display();
+    return 0;
+}
