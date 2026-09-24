19f893afbe12bb8b42673cbcc75af72ebbb07c12
rushidalavi2007-lang <rushidalavi2007@gmail.com>
2026-09-24 16:24:12 +0530

    live example 15

---
+++ U4_Live_Example_03.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+using namespace std;
+
+struct ImageMeta {
+    int width;
+    int height;
+};
+
+int main() {
+    ImageMeta img1 = {1920, 1080};
+    ofstream out("img.bin", ios::binary);
+    out.write((char*)&img1, sizeof(ImageMeta));
+    out.close();
+
+    ImageMeta img2;
+    ifstream in("img.bin", ios::binary);
+    in.read((char*)&img2, sizeof(ImageMeta));
+    in.close();
+
+    cout << "== Binary Metadata Read ==" << endl;
+    cout << "Resolution: " << img2.width << " x " << img2.height << endl;
+    return 0;
+}
