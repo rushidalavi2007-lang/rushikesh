79999d116acd405ee791013aaa555503282bd9a6
rushidalavi2007-lang <rushidalavi2007@gmail.com>
2026-09-24 16:26:05 +0530

    live example 21

---
+++ U6_Live_Example_01.cpp
@@ -0,0 +1,25 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <algorithm>
+using namespace std;
+
+struct Employee {
+    string name;
+    int age;
+    double salary;
+};
+
+int main() {
+    vector<Employee> staff = {{"Alice", 30, 70000}, {"Bob", 25, 50000}};
+    map<string, double> salaryLookup;
+
+    for (int i = 0; i < staff.size(); i++) {
+        salaryLookup[staff[i].name] = staff[i].salary;
+    }
+
+    cout << "== STL Map Salary Lookup ==" << endl;
+    cout << "Bob's Salary: Rs. " << salaryLookup["Bob"] << endl;
+    return 0;
+}
