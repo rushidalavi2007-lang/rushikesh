a67278ba237f93a5e87e3c6552f4618c419eccb8
rushidalavi2007-lang <rushidalavi2007@gmail.com>
2026-09-24 16:26:20 +0530

    live example 22

---
+++ U6_Live_Example_02.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+using namespace std;
+
+int main() {
+    vector<string> ips = {"192.168.1.1", "192.168.1.2", "192.168.1.1", "192.168.1.3", "192.168.1.1"};
+    map<string, int> freq;
+
+    for (int i = 0; i < ips.size(); i++) {
+        freq[ips[i]]++;
+    }
+
+    cout << "== IP Request Frequencies ==" << endl;
+    for (auto it = freq.begin(); it != freq.end(); it++) {
+        cout << it->first << ": " << it->second << " requests" << endl;
+    }
+    return 0;
+}
