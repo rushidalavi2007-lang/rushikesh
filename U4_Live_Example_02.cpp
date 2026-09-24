6d4552acce77ddda794cec16dffe4da0e577034c
rushidalavi2007-lang <rushidalavi2007@gmail.com>
2026-09-24 16:23:05 +0530

    live example 14

---
+++ U4_Live_Example_02.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+using namespace std;
+
+int main() {
+    ofstream logOut("server.log");
+    logOut << "INFO Server started\n";
+    logOut << "ERROR Database connection failed\n";
+    logOut << "CRITICAL Disk space low\n";
+    logOut.close();
+
+    ifstream logIn("server.log");
+    string line;
+    cout << "== Critical DevOps Events ==" << endl;
+    while (getline(logIn, line)) {
+        if (line.find("ERROR") != string::npos || line.find("CRITICAL") != string::npos) {
+            cout << line << endl;
+        }
+    }
+    logIn.close();
+    return 0;
+}
