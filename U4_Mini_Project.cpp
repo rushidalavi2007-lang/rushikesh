2c288fbde522bdf03e58a3259760f4fbb3a22509
rushidalavi2007-lang <rushidalavi2007@gmail.com>
2026-09-24 16:24:29 +0530

    live example16

---
+++ U4_Mini_Project.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+#include <fstream>
+#include <string>
+using namespace std;
+
+class Library {
+public:
+    static void addBook(int id, string title) {
+        ofstream out("books.txt", ios::app);
+        out << id << " " << title << "\n";
+        out.close();
+    }
+
+    static void showBooks() {
+        ifstream in("books.txt");
+        int id; string title;
+        cout << "== Library Inventory ==" << endl;
+        while (in >> id >> title) {
+            cout << "Book ID: " << id << " | Title: " << title << endl;
+        }
+        in.close();
+    }
+};
+
+int main() {
+    Library::addBook(1, "OOP_Concepts");
+    Library::addBook(2, "Data_Structures");
+    Library::showBooks();
+    return 0;
+}
