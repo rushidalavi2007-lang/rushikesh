d8309f3e8a1680e929d38219fee999fabbcdff3c
rushidalavi2007-lang <rushidalavi2007@gmail.com>
2026-09-25 22:42:51 +0530

    code35.cpp

---
+++ code35.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+using namespace std;
+
+class Shape {
+public:
+    virtual double area() const = 0;
+    virtual ~Shape() = default;
+};
+
+class Rectangle : public Shape {
+private:
+    double length;
+    double width;
+public:
+    Rectangle(double givenLength, double givenWidth)
+        : length(givenLength), width(givenWidth) {}
+
+    double area() const override {
+        return length * width;
+    }
+};
+
+int main() {
+    Rectangle rectangle(8.0, 4.0);
+    cout << "Rectangle Area: " << rectangle.area() << '\n';
+    return 0;
+}
