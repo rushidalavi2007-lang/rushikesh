0d23f7c5665453d610532f53b412c1ec21ea7d17
rushidalavi2007-lang <rushidalavi2007@gmail.com>
2026-09-25 22:41:02 +0530

    code30.cpp

---
+++ code30.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class Distance {
+private:
+    int meters;
+public:
+    explicit Distance(int value) : meters(value) {}
+
+    bool operator>(const Distance& other) const {
+        return meters > other.meters;
+    }
+
+    void display() const {
+        cout << meters << " meters\n";
+    }
+};
+
+int main() {
+    Distance first(120);
+    Distance second(90);
+
+    cout << "First distance: ";
+    first.display();
+
+    cout << "Second distance: ";
+    second.display();
+
+    if (first > second) {
+        cout << "First distance is greater\n";
+    } else {
+        cout << "Second distance is greater or equal\n";
+    }
+
+    return 0;
+}
